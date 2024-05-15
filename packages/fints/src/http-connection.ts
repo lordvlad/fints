999073930fceda6e1941418ee6bcfafdb48eacb1
waldemar.reusch <waldemar.reusch@gmail.com>
2024-05-15 23:04:24 +0000

    fix node-gyp version; allow proxy override by parameters

--- packages/fints/src/http-connection.ts
+++ packages/fints/src/http-connection.ts
@@ -1,50 +1,58 @@
 import "isomorphic-fetch";
 import { verbose } from "./logger";
 import { encodeBase64, decodeBase64 } from "./utils";
-import { Request } from "./request";
+import type { Request } from "./request";
 import { Response } from "./response";
-import { Connection } from "./types";
+import type { Connection } from "./types";
 
 /**
  * Configuration specifying how to reach the fints server.
  */
 export class ConnectionConfig {
-    /**
-     * The URL to reach the server at.
-     */
-    public url: string;
-    /**
-     * If set to `true`, will log all requests performed and responses received.
-     */
-    public debug = false;
+  /**
+   * The URL to reach the server at.
+   */
+  public url: string;
+  /**
+   * If set to `true`, will log all requests performed and responses received.
+   */
+  public debug = false;
+  /**
+   * If set, will use proxy for HTTP calls and set the actual url in TARGET header.
+   */
+  public proxy?: string;
 }
 
 /**
  * A connection used by clients to reach the fints server.
  */
 export class HttpConnection extends ConnectionConfig implements Connection {
-    constructor(config: ConnectionConfig) {
-        super();
-        Object.assign(this, config);
+  constructor(config: ConnectionConfig) {
+    super();
+    Object.assign(this, config);
+  }
+
+  public async send(request: Request): Promise<Response> {
+    const { url, proxy, debug } = this;
+    verbose(`Sending Request: ${request}`);
+    if (debug) verbose(`Parsed Request:\n${request.debugString}`);
+
+    const method = "POST";
+    const body = encodeBase64(String(request));
+    const headers = { TARGET: url };
+    const httpRequest = proxy
+      ? await fetch(proxy, { method, body, headers })
+      : await fetch(url, { method, body });
+    if (!httpRequest.ok) {
+      throw new Error(
+        `Received bad status code ${httpRequest.status} from FinTS endpoint.`,
+      );
     }
 
-    public async send(request: Request): Promise<Response> {
-        const { url } = this;
-        verbose(`Sending Request: ${request}`);
-        if (this.debug) { verbose(`Parsed Request:\n${request.debugString}`); }
-         
-        const method = "POST"
-        const body = encodeBase64(String(request))
-        const headers = {"TARGET": url}
-        const httpRequest = (typeof window === "undefined")
-            ? await fetch(url, { method, body })
-            : await fetch("http://localhost:3000",{ method, body, headers});
-        if (!httpRequest.ok) { throw new Error(`Received bad status code ${httpRequest.status} from FinTS endpoint.`); }
-
-        const responseString = decodeBase64(await httpRequest.text());
-        verbose(`Received Response: ${responseString}`);
-        const response = new Response(responseString);
-        if (this.debug) { verbose(`Parsed Response:\n${response.debugString}`); }
-        return response;
-    }
+    const responseString = decodeBase64(await httpRequest.text());
+    verbose(`Received Response: ${responseString}`);
+    const response = new Response(responseString);
+    if (debug) verbose(`Parsed Response:\n${response.debugString}`);
+    return response;
+  }
 }
--- packages/fints/src/pin-tan-client.ts
+++ packages/fints/src/pin-tan-client.ts
@@ -1,67 +1,87 @@
 import { Client } from "./client";
-import { Dialog, DialogConfig } from "./dialog";
+import { Dialog, type DialogConfig } from "./dialog";
 import { Request } from "./request";
 import { HttpConnection } from "./http-connection";
-import { Segment } from "./segments";
-import { Connection } from "./types";
+import type { Segment } from "./segments";
+import type { Connection } from "./types";
 import { PRODUCT_NAME } from "./constants";
 
 /**
  * Set of options needed to construct a `PinTanClient`.
  */
 export interface PinTanClientConfig {
-    /**
-     * The fints product identification.
-     */
-    productId?: string;
-    /**
-     * The banks identification number (Bankleitzahl).
-     */
-    blz: string;
-    /**
-     * The username or identification number.
-     */
-    name: string;
-    /**
-     * The pin code or password used for authenticating with the fints server.
-     */
-    pin: string;
-    /**
-     * The URL to reach the server at.
-     */
-    url: string;
-    /**
-     * If set to `true`, will log all requests performed and responses received.
-     */
-    debug?: boolean;
+  /**
+   * The fints product identification.
+   */
+  productId?: string;
+  /**
+   * The banks identification number (Bankleitzahl).
+   */
+  blz: string;
+  /**
+   * The username or identification number.
+   */
+  name: string;
+  /**
+   * The pin code or password used for authenticating with the fints server.
+   */
+  pin: string;
+  /**
+   * The URL to reach the server at.
+   */
+  url: string;
+  /**
+   * If set to `true`, will log all requests performed and responses received.
+   */
+  debug?: boolean;
+  /**
+   * If set, will use proxy for HTTP calls and set the actual url in TARGET header.
+   */
+  proxy?: string;
 }
 
 export class PinTanClient extends Client {
-    /**
-     * Connection used to reach the server.
-     */
-    private connection: Connection;
-    /**
-     * Configuration for connecting and authenticating.
-     */
-    protected config: PinTanClientConfig;
+  /**
+   * Connection used to reach the server.
+   */
+  private connection: Connection;
+  /**
+   * Configuration for connecting and authenticating.
+   */
+  protected config: PinTanClientConfig;
 
-    constructor(config: PinTanClientConfig) {
-        super();
-        this.config = config;
-        const { url, debug } = config;
-        this.connection = new HttpConnection({ url, debug });
-    }
+  constructor(config: PinTanClientConfig) {
+    super();
+    this.config = config;
+    const { url, debug, proxy } = config;
+    this.connection = new HttpConnection({ url, debug, proxy });
+  }
 
-    public createDialog(dialogConfig?: DialogConfig) {
-        const { blz, name, pin, productId = PRODUCT_NAME } = this.config;
-        const { connection } = this;
-        return new Dialog(dialogConfig ? dialogConfig : { blz, name, pin, systemId: "0", productId }, connection);
-    }
+  public createDialog(dialogConfig?: DialogConfig) {
+    const { blz, name, pin, productId = PRODUCT_NAME } = this.config;
+    const { connection } = this;
+    return new Dialog(
+      dialogConfig
+        ? dialogConfig
+        : { blz, name, pin, systemId: "0", productId },
+      connection,
+    );
+  }
 
-    public createRequest(dialog: Dialog, segments: Segment<any>[], tan?: string) {
-        const { blz, name, pin } = this.config;
-        const { systemId, dialogId, msgNo, tanMethods } = dialog;
-        return new Request({ blz, name, pin, systemId, dialogId, msgNo, segments, tanMethods, tan });
-    }
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  public createRequest(dialog: Dialog, segments: Segment<any>[], tan?: string) {
+    const { blz, name, pin } = this.config;
+    const { systemId, dialogId, msgNo, tanMethods } = dialog;
+    return new Request({
+      blz,
+      name,
+      pin,
+      systemId,
+      dialogId,
+      msgNo,
+      segments,
+      tanMethods,
+      tan,
+    });
+  }
 }
